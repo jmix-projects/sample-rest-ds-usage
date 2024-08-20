5f14bc50eec03ffb5d79131a40a9681436966023
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-08-20 16:59:34 +0400

    Order JPA entity, itemsQuery for Customer

---
+++ orders/src/main/java/com/company/orders/entity/Order.java
@@ -0,0 +1,89 @@
+package com.company.orders.entity;
+
+import com.company.orders.entity.customer.Customer;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.SystemLevel;
+import io.jmix.core.metamodel.annotation.DependsOnProperties;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import io.jmix.core.metamodel.annotation.JmixProperty;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "ORDER_")
+@Entity(name = "Order_")
+public class Order {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @Column(name = "NUMBER_")
+    private String number;
+
+    @Column(name = "DATE_")
+    private LocalDate date;
+
+    @SystemLevel
+    @Column(name = "CUSTOMER_ID")
+    private UUID customerId;
+
+    @Transient
+    @JmixProperty
+    @DependsOnProperties("customerId")
+    private Customer customer;
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(UUID customerId) {
+        this.customerId = customerId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
---
+++ orders/src/main/java/com/company/orders/view/order/OrderDetailView.java
@@ -0,0 +1,16 @@
+package com.company.orders.view.order;
+
+import com.company.orders.entity.Order;
+import com.company.orders.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.EditedEntityContainer;
+import io.jmix.flowui.view.StandardDetailView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route(value = "orders/:id", layout = MainView.class)
+@ViewController("Order_.detail")
+@ViewDescriptor("order-detail-view.xml")
+@EditedEntityContainer("orderDc")
+public class OrderDetailView extends StandardDetailView<Order> {
+}
